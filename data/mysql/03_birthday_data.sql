af896ffbb586e50ff982814ff03b914eaf296be2
luka-lta <luka.lta21@gmail.com>
2024-02-26 00:19:24 +0100

    #1 birthday repository added

--- data/mysql/03_birthday_data.sql
+++ data/mysql/03_birthday_data.sql
@@ -1,8 +1,15 @@
-CREATE TABLE `birthday_data` (
-                                 `user_id` bigint NOT NULL,
-                                 `birthday` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `birthday_data`
+(
+    `guild_id` bigint NOT NULL,
+    `user_id`  bigint NOT NULL,
+    `birthday` date   NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
 ALTER TABLE `birthday_data`
-    ADD PRIMARY KEY (`user_id`);
-COMMIT;
+    ADD PRIMARY KEY (`user_id`),
+    ADD UNIQUE KEY `guild_id` (`guild_id`);
+
+ALTER TABLE `birthday_data`
+    ADD CONSTRAINT `Guild` FOREIGN KEY (`guild_id`) REFERENCES `config_data` (`guild_id`) ON DELETE CASCADE ON UPDATE CASCADE;
